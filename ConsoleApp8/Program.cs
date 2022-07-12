031930458ce558168f5fd84d2bbb6d80149adef0
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-12 19:36:02 +0300

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int players = 1;
+            string missionName = "Нападение на дом";
+            float missionComplete = 10.2f;
+            double healthPoints = 85.60d;
+            bool playerIsDeath = false;
+            byte numberOfRooms = 8;
+            sbyte numberOfFloors = 2;
+            short numberOfRounds = 90;
+            ushort numberOfWeapons = 4;
+            char firstSymbol = 'Q';
+        }
+    }
+}
